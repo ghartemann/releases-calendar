3e7dd17a0b6621354d3a3336112f0b8a4467e7aa
Guillaume Hartemann-Piollet <36921636+ghartemann@users.noreply.github.com>
2023-05-19 00:03:34 +0200

    Initial commit

---
+++ assets/app.js
@@ -0,0 +1,25 @@
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+
+import { createApp } from 'vue';
+import App from '@pages/App.vue';
+
+import router from "./js/router";
+
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+createApp(App)
+    .use(router)
+    .use(vuetify)
+    .mount('#app');
